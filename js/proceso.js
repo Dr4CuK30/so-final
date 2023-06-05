4ff1d7f19ba8e62dd715a0d879fe83af6eda588b
Santiago Herrerea <santiherreravelas@gmail.com>
2023-06-04 20:17:47 -0500

    SRTF added

--- js/proceso.js
+++ js/proceso.js
@@ -6,57 +6,58 @@ let SUSPENDIDO = "Suspendido";
 let TERMINADO = "Terminado";
 let cont = 0;
 
-function Proceso(lleg, pri=0, raf=Math.floor(Math.random() * 7) + 3, nom="P"){
-	if(nom == "P"){
-		this.nombre = nom + cont++;
-	}else{
-		this.nombre = nom;
-	}
-	this.llegada = lleg;
-	this.rafaga = raf;
-	this.rafaga_res = raf - 1;  // Rafaga restante
-	this.comienzo = 0;
-	this.final = 0;
-	this.retorno = 0;
-	this.espera = 0;
-	this.bloqueo = 0;
-	this.suspendido = 0;
+function Proceso(lleg, pri = 0, raf = Math.floor(Math.random() * 7) + 3, nom = "P") {
+    if (nom == "P") {
+        this.nombre = nom + cont++;
+    } else {
+        this.nombre = nom;
+    }
+    this.llegada = lleg;
+    this.rafaga = raf;
+    this.rafaga_res = raf - 1; // Rafaga restante
+    this.rafaga_ini = raf; // Rafaga inicial
+    this.comienzo = 0;
+    this.final = 0;
+    this.retorno = 0;
+    this.espera = 0;
+    this.bloqueo = 0;
+    this.suspendido = 0;
 
-	this.prioridad = pri;
-	this.estado = ESPERA;
+    this.prioridad = pri;
+    this.estado = ESPERA;
 
-	// Para diagrama de Gantt
-	this.fila = []
-	/*
-	 * Va a estar lleno con numeros
-	 * 0 -> Estado de espera
-	 * 1 -> Estado de ejecucion
-	 * 2 -> Estado bloqueado
-	 * 3 -> Estado suspendido
-	 */
+    // Para diagrama de Gantt
+    this.fila = []
+        /*
+         * Va a estar lleno con numeros
+         * 0 -> Estado de espera
+         * 1 -> Estado de ejecucion
+         * 2 -> Estado bloqueado
+         * 3 -> Estado suspendido
+         */
 
-	this.calcular = (com) => {
-		this.comienzo = com;
-		this.final = this.rafaga + this.comienzo;
-		this.retorno = this.final - this.llegada;
-		this.espera = this.retorno - this.rafaga;
-	}
+    this.calcular = (com) => {
+        this.comienzo = com;
+        this.final = this.rafaga + this.comienzo;
+        this.retorno = this.final - this.llegada;
+        this.espera = this.retorno - this.rafaga;
+    }
 
-	this.bloquear = () => {
-		if(this.estado == EJECUCION){
-			this.bloqueo = 4;
-			this.estado = BLOQUEO;
-			this.rafaga -= this.rafaga_res + 1;
-			this.calcular(this.llegada);
-		}
-	}
+    this.bloquear = () => {
+        if (this.estado == EJECUCION) {
+            this.bloqueo = 4;
+            this.estado = BLOQUEO;
+            this.rafaga -= this.rafaga_res + 1;
+            this.calcular(this.llegada);
+        }
+    }
 
-	this.suspender = () => {
-		if(this.estado == EJECUCION){
-			this.suspendido = 1;
-			this.estado = SUSPENDIDO;
-			this.rafaga -= this.rafaga_res + 1;
-			this.calcular(this.llegada);
-		}
-	}
+    this.suspender = () => {
+        if (this.estado == EJECUCION) {
+            this.suspendido = 1;
+            this.estado = SUSPENDIDO;
+            this.rafaga -= this.rafaga_res + 1;
+            this.calcular(this.llegada);
+        }
+    }
 }
